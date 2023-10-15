afce28f466f0293d878abfd185e7f8991cdbf5e6
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-10-15 13:49:52 +0300

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -3,5 +3,16 @@ set -o allexport; source .env; set +o allexport;
 
 #wait until the server is ready
 echo "Waiting for software to be ready ..."
-sleep 30s;
+sleep 60s;
 
+target=$(docker-compose port web 3000)
+
+curl https//${target}/api/admin/accounts/create \
+  -H 'accept: */*' \
+  -H 'accept-language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,he;q=0.6' \
+  -H 'cache-control: no-cache' \
+  -H 'content-type: text/plain;charset=UTF-8' \
+  -H 'pragma: no-cache' \
+  -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36' \
+  --data-raw '{"username":"admin","password":"'${ADMIN_PASSWORD}'"}' \
+  --compressed
--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -1,3 +1,22 @@
 #set env vars
 set -o allexport; source .env; set +o allexport;
 
+sed -i "s~DOMAIN~${DOMAIN}~g" ./.config/default.yml
+sed -i "s~PASSWORD~${POSTGRES_PASSWORD}~g" ./.config/default.yml
+
+cat <<EOT > ./servers.json
+{
+    "Servers": {
+        "1": {
+            "Name": "local",
+            "Group": "Servers",
+            "Host": "172.17.0.1",
+            "Port": 43282,
+            "MaintenanceDB": "postgres",
+            "SSLMode": "prefer",
+            "Username": "postgres",
+            "PassFile": "/pgpass"
+        }
+    }
+}
+EOT
